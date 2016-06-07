e038b0f3a614996236f5988055843f87883a102b
vigkre <savikram007@gmail.com>
2016-06-07 20:09:31 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+     
+    int main(void) {
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    printf("P");
+    else if(n<0)
+    printf("N");
+    else 
+    printf("Z");
+     
+    	return 0;
+    }
+     
