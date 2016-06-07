3dc4eeb4a303ecff62311ff3df6ab464b56ec5f7
vigkre <savikram007@gmail.com>
2016-06-07 16:00:23 +0530

    Create pos.cpp

---
+++ pos.cpp
@@ -0,0 +1,14 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int n;
+    	cin>>n;
+    	if(n>0)
+    	cout<<"Positive number";
+    	else if(n<0)
+    	cout<<"Negative number";
+    	else 
+    	cout<<"Zero";// your code goes here
+    	return 0;
+    }
