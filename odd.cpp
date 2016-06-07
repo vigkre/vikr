5195353aeed4451ed810062dd9a0cff6a20cafb1
vigkre <savikram007@gmail.com>
2016-06-07 20:36:49 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,15 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int m,i,n;
+    	cin>>m>>n;
+    	for(i=m;i<=n;i++)
+    	{
+    		if(i%2!=0)
+    		{	cout<<i<<" ";
+    		cout<<"\t";}
+    	}
+     
+    	return 0;
+    }
