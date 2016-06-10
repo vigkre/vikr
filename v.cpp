d73562f8426d3f83d7bdabcf2cae8cd748c7dc8d
vigkre <savikram007@gmail.com>
2016-06-10 13:56:02 +0530

    Create v.cpp

---
+++ v.cpp
@@ -0,0 +1,14 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    int m,sum=0;
+    cin>>m;
+    while(m)
+    {
+    sum+=m;
+    m--;
+    }
+    cout<<sum;
+    return 0;
+    }
